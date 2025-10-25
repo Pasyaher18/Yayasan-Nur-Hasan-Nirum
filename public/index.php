d656edb228a69c653a45d3d77cc3376249ef554e
Muhammad Pasya Hermansyah <Pasyahermansyah11@gmail.com>
2025-10-25 15:31:57 +0700

    Update index.php for InfinityFree deployment

--- public/index.php
+++ public/index.php
@@ -5,16 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// Cek mode maintenance
+if (file_exists($maintenance = __DIR__.'/../yayasan/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// Load autoloader Composer
+require __DIR__.'/../yayasan/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap Laravel
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../yayasan/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
